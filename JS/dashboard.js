1241442f853a917aa8c0a0b7bb69ea993d990d16
Eloghosa Jeremiah Nosakhare <eloghosa.nosakhare@techskool.org>
2025-07-08 22:22:09 +0100

    Added new files to the dashboard folder
    I added new files to the dadhboard to the dashboard folder, css folder, js folder

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,8 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('but-div').addEventListener('click', function(){
-        let newText = document.createElement('h1')
-        newText.innerText = "Redirecting to login page";
-        document.appendChild('article', 'newText');
-        window.location.href = "../login.html";
+function loadComponent(id, file) {
+  return fetch(file)
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
     })
-})
+    .catch((error) => {
+      console.error(`Error loading ${file}:`, error);
+      document.getElementById(id).innerHTML = `<p>Error loading ${file}</p>`;
+    });
+}
+
+// function loadComponent(id, file) {
+//   return fetch(file)
+//     .then((res) => res.text())
+//     .then((data) => {
+//       document.getElementById(id).innerHTML = data;
+//       if (id === "header") {
+//         const links = document.querySelectorAll("#header nav div a");
+//         links.forEach((link) => {
+//           if (
+//             link.href === window.location.href ||
+//             link.pathname === window.location.pathname
+//           ) {
+//             link.classList.add("active");
+//           }
+//         });
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(`Error loading ${file}:`, error);
+//       document.getElementById(id).innerHTML = `<p>Error loading ${file}</p>`;
+//     });
+// }
+window.addEventListener("load", () => {
+  Promise.all([
+    loadComponent("header", "dashboardNav.html"),
+    loadComponent("footer", "footer.html"),
+  ]).then(() => {
+    setTimeout(() => {
+      const loader = document.getElementById("loading");
+      if (loader) loader.style.display = "none";
+    }, 200);
+  });
+});
