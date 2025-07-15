bdd470042545a7b4a9d2ef9e73c85caa4428d1b5
Soyinka4gabriel <142997790+Soyinka4gabriel@users.noreply.github.com>
2025-07-15 15:10:56 +0100

    Exercise and Quiz
    for Exercise here is the map
    project/
    │
    ├──Exercise.html
    ├── JS
           ├──  questions.js
           ├── exercise.js
    
    for Quiz here is the map
    project/
    │
    ├──quizz.html
    ├── JS
           ├──  questions.js
           ├── quizz.js

---
+++ JS/question.js
@@ -0,0 +1,34 @@
+const testData = {
+  questions: [
+    {
+      id: "q1",
+      text: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4",
+    },
+    {
+      id: "q2",
+      text: "Capital of France?",
+      options: ["London", "Berlin", "Paris", "Rome"],
+      answer: "Paris",
+    },
+    {
+      id: "q3",
+      text: "5 x 5 = ?",
+      options: ["10", "20", "25", "30"],
+      answer: "25",
+    },
+    {
+      id: "q4",
+      text: "Color of the sky?",
+      options: ["Red", "Green", "Blue", "Yellow"],
+      answer: "Blue",
+    },
+    {
+      id: "q5",
+      text: 'Opposite of "cold"?',
+      options: ["Cool", "Warm", "Hot", "Ice"],
+      answer: "Hot",
+    },
+  ],
+};
